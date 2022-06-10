73c300acdd29fa35cb4926a47b50c25b84e4cb24
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-10 11:02:06 +0300

    commit tree cb372f9874e8e9d3699802633f3110c573671e8d

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name){
-    cats.push(name);
-}
+
 
 function destructivelyPrependCat(name) {
 
